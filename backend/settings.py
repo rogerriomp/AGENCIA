77951289a7b360b8edf23dc5663b10c4558e25bc
root <root@srv-iprev2.intranet.ciasc.gov.br>
2021-07-06 12:05:31 -0300

    Updata settings params

--- backend/settings.py
+++ backend/settings.py
@@ -1,6 +1,6 @@
 import sqlite3
 import pandas as pd
-import ast
+import ast as ast
 from sqlalchemy import create_engine
 
 #engine = create_engine('postgresql://postgres:55262408@172.16.200.237:5432/postgres')
