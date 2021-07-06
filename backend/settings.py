9df910077633d45d7d7e24666a9f753a150814c6
root <root@srv-iprev2.intranet.ciasc.gov.br>
2021-07-06 12:08:56 -0300

    Update settings params

--- backend/settings.py
+++ backend/settings.py
@@ -4,4 +4,4 @@ import ast as ast
 from sqlalchemy import create_engine
 
 #engine = create_engine('postgresql://postgres:55262408@172.16.200.237:5432/postgres')
-engine = create_engine('postgresql://postgres:55262408@localhost:5432/expensao')
+engine = create_engine('postgresql://root:55262408@localhost:5432/expensao')
